4cb036215842376333314533beaf9d83ba200282
GabrielDJVC <djmgabriel@outlook.com>
2023-12-05 17:10:49 +0100

    commmit

--- Utilities/Utilities.php
+++ Utilities/Utilities.php
@@ -3,16 +3,11 @@
 function openFile($pFile)
 {
     $file = fopen($pFile, "r");
-
-    $i = 0;
     $arra_lines = [];
     while (($ligne = fgets($file)) != false) {
-        $arra_lines[$i] = $ligne;
-        $i++;
+        $arra_lines[] = $ligne;
     }
-    /* echo '<pre>';
-    var_dump($arra_lines);
-    echo '</pre>'; */
+   
     return $arra_lines;
 }
 
