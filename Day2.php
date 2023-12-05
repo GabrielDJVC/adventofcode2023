7640266c4bb527882b6e8abade5876840e9373bf
GabrielDJVC <djmgabriel@outlook.com>
2023-12-05 13:09:35 +0100

    commit

--- Day2.php
+++ Day2.php
@@ -8,6 +8,8 @@ $arra_payload = openFile("Files/file_day_2.txt");
 
 $arra_new = explode(":", $value);
 $total_value = 0;
+$set_power = 1;
+$total_value_2 = 0;
 
 foreach ($arra_payload as $key => $value) {
 
@@ -23,6 +25,7 @@ foreach ($arra_payload as $key => $value) {
         'green' => 13,
         'blue' =>  14,
     ];
+
     $color_not_gt = true;
     foreach ($arra_colors as $value) {
         $colors = explode(',', $value);
@@ -40,19 +43,42 @@ foreach ($arra_payload as $key => $value) {
         $color_max = max($arra_final[$game][$key]);
 
         if ($color_max > $value) {
+
+            $set_power = $color_max * $set_power;
+
+
             $color_not_gt = false;
             break;
         }
+        $set_power = $color_max * $set_power;
     }
+
     if ($color_not_gt) {
         $total_value += (int)substr($game, 4, strlen($game));
     }
+
+    $set_power = 1;
+    foreach ($arra_val_to_compare as $key => $value) {
+        $color_max = max($arra_final[$game][$key]);
+
+        $set_power = $color_max * $set_power;
+    }
+
+    $total_value_2 += $set_power;
+   
+
 }
-echo '<pre>';
+echo '<pre> Day 2.1';
 var_dump($total_value);
 echo '</pre>';
 
-/* echo '<pre>';
+echo '<pre> Day 2.2';
+var_dump($total_value_2);
+echo '</pre>';
+
+
+
+echo '<pre>';
 var_dump($arra_final);
-echo '</pre>'; */
+echo '</pre>'; 
 // 12 red cubes, 13 green cubes, and 14 blue cubes
