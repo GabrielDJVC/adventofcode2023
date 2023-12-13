d523428f5eb76e13a4f087e77b0e0059acdc0a31
GabrielDJVC <djmgabriel@outlook.com>
2023-12-13 16:46:36 +0100

    Day 4 part 1

---
+++ Day4.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once("Utilities/Utilities.php");
+
+
+$arr_payload = openFile("Files/day4.txt");
+
+$points_finaux = 0;
+
+foreach ($arr_payload as $value) {
+
+    $separator = explode(":", $value);
+
+    $card_nb = substr(str_replace(" ", "", trim($separator[0])), 4);
+
+    list($part1, $part2) = explode("|", trim($separator[1]));
+
+    $arr_part1 = explode(" ", $part1);
+    $arr_part2 = explode(" ", $part2);
+
+    $multi  = 2;
+
+
+    $first_match = true;
+    $points = 0;
+
+    foreach (array_filter($arr_part1) as $value) {
+        if (in_array($value, $arr_part2)) {
+            if($first_match){
+                $points += 1;
+               
+                $first_match = false;
+            }else {
+                $points *= $multi;
+            }
+        }
+    }
+    $points_finaux += $points;
+ 
+
+}
+
+echo '<pre> ';
+var_dump($points_finaux);
+echo '</pre>';
+
