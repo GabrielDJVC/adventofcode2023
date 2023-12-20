c2845deedc8a43a88216112f3e423722d25f98e5
GabrielDJVC <djmgabriel@outlook.com>
2023-12-20 14:48:04 +0100

    commit

--- Day9.php
+++ Day9.php
@@ -9,7 +9,7 @@ $answer1 = 0;
 $answer2 = 0;
 
 foreach ($arr_payload as $value) {
-    
+
     $arr_values_to_add = [];
     $arr_value = array_map('intval', explode(" ", $value));
 
@@ -19,7 +19,6 @@ foreach ($arr_payload as $value) {
     $value_added = addNumber(array_reverse($result));
 
     $value_added_reverse = addNumberReverse(array_reverse($result));
-    exit;
 }
 
 
@@ -57,31 +56,33 @@ function addNumber($array)
                 $last_index_n = count($array[$i]) - 1;
                 $last_index_n_before = count($array[$i - 1]) - 1;
 
-                $array[$i][] = $array[$i][$last_index_n] + $array[$i - 1][$last_index_n_before] ;
+                $array[$i][] = $array[$i][$last_index_n] + $array[$i - 1][$last_index_n_before];
             }
             $array[0][] = 0;
         }
     }
-    $last_index_of_last_array = count($array[$last_key]) -1 ;
+    $last_index_of_last_array = count($array[$last_key]) - 1;
     $answer1 += $array[$last_key][$last_index_of_last_array];
-
 }
 
 
 function addNumberReverse($array)
 {
     global $answer2;
+    $last_key = count($array) - 1;
 
     for ($i = 0; $i < count($array); $i++) {
 
         if ($i != 0) {
-            
-            array_unshift($array[$i],7); // A AJOUTER LA SOUSTRACTION
+            $val_to_add = ($array[$i][0] - $array[$i - 1][0]);
+            array_unshift($array[$i], $val_to_add); // A AJOUTER LA SOUSTRACTION
         }
         $array[0][] = 0;
-        echo "String $i: " . implode(" ", $array[$i]) . "<br />";
-
+        //echo "String $i: " . implode(" ", $array[$i]) . "<br />";
     }
+    $answer2 += $array[$last_key][0];
 }
 
+
 echo '<pre> <strong>Partie 1</strong> : ' . $answer1 . '</pre> ';
+echo '<pre> <strong>Partie 2</strong> : ' . $answer2 . '</pre> ';
